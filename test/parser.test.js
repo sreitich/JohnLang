1231a384b3cef0d331718e973f98db078cfd9819
Sam Reitich <sam.reitich@gmail.com>
2025-02-20 12:20:10 -0800

    Added syntactically incorrect parser tests
    Made minor changes to grammar to pass new tests.
    
    All parser tests (syntactically correct and incorrect) pass successfully.

--- test/parser.test.js
+++ test/parser.test.js
@@ -168,6 +168,10 @@ const syntaxChecks = [
         letMeLearnYouSomething(nah youBetcha || nah thinkAgainPal)!
     `],
 
+    /**
+     * NOTE: Currently, JohnLang allows JavaScript-like mixing of ands and ors. If we decide
+     * to change this feature, the third line of this test case must be changed.
+     */
     ["Conditional chaining", `
         letMeLearnYouSomething(a || b || c || d)!
         letMeLearnYouSomething(a && b && c && d)!
@@ -253,41 +257,167 @@ const syntaxChecks = [
 
 // Programs with syntax errors that the parser will detect
 const syntaxErrors = [
-    ["non-letter in an identifier", "let ab😭c = 2;", /Line 1, col 7:/],
+    ["Non-letter in an identifier",
+        "handful ab😭c: 2!",
+            /Line 1, col 11:/],
 
-    // TODO: Finish:
-    // ["malformed number", "let x= 2.;", /Line 1, col 10:/],
-    // ["a float with an E but no exponent", "let x = 5E * 11;", /Line 1, col 10:/],
-    // ["a missing right operand", "print(5 -);", /Line 1, col 10:/],
-    // ["a non-operator", "print(7 * ((2 _ 3));", /Line 1, col 15:/],
-    // ["an expression starting with a )", "return );", /Line 1, col 8:/],
-    // ["a statement starting with expression", "x * 5;", /Line 1, col 3:/],
-    // ["an illegal statement on line 2", "print(5);\nx * 5;", /Line 2, col 3:/],
-    // ["a statement starting with a )", "print(5);\n)", /Line 2, col 1:/],
-    // ["an expression starting with a *", "let x = * 71;", /Line 1, col 9:/],
-    // ["negation before exponentiation", "print(-2**2);", /Line 1, col 10:/],
-    // ["mixing ands and ors", "print(1 && 2 || 3);", /Line 1, col 15:/],
-    // ["mixing ors and ands", "print(1 || 2 && 3);", /Line 1, col 15:/],
-    // ["associating relational operators", "print(1 < 2 < 3);", /Line 1, col 13:/],
-    // ["while without braces", "while true\nprint(1);", /Line 2, col 1/],
-    // ["if without braces", "if x < 3\nprint(1);", /Line 2, col 1/],
-    // ["while as identifier", "let for = 3;", /Line 1, col 5/],
-    // ["if as identifier", "let if = 8;", /Line 1, col 5/],
-    // ["unbalanced brackets", "function f(): int[;", /Line 1, col 18/],
-    // ["empty array without type", "print([]);", /Line 1, col 8/],
-    // ["random used like a function", "print(random(1,2));", /Line 1, col 15/],
-    // ["bad array literal", "print([1,2,]);", /Line 1, col 12/],
-    // ["empty subscript", "print(a[]);", /Line 1, col 9/],
-    // ["true is not assignable", "true = 1;", /Line 1, col 5/],
-    // ["false is not assignable", "false = 1;", /Line 1, col 6/],
-    // ["numbers cannot be subscripted", "print(500[x]);", /Line 1, col 10/],
-    // ["numbers cannot be called", "print(500(x));", /Line 1, col 10/],
-    // ["numbers cannot be dereferenced", "print(500 .x);", /Line 1, col 11/],
-    // ["no-paren function type", "function f(g:int->int) {}", /Line 1, col 17/],
-    // ["string lit with unknown escape", 'print("ab\\zcdef");', /col 11/],
-    // ["string lit with newline", 'print("ab\\zcdef");', /col 11/],
-    // ["string lit with quote", 'print("ab\\zcdef");', /col 11/],
-    // ["string lit with code point too long", 'print("\\u{1111111}");', /col 17/],
+    ["Bad number formatting",
+        "handful x: 2.!",
+            /Line 1, col 14:/],
+
+    ["A float with an E but no exponent",
+        "handful x: 5E * 11!",
+            /Line 1, col 13:/],
+
+    ["A missing right operand",
+        "letMeLearnYouSomething(5 -)!",
+            /Line 1, col 27:/],
+
+    ["A non-operator",
+        "letMeLearnYouSomething(7 * ((2 _ 3))!",
+            /Line 1, col 32:/],
+
+    ["An expression starting with an open parenthesis",
+        "betterGetGoin )!",
+            /Line 1, col 15:/],
+
+    ["A statement starting with expression",
+        "x * 5!",
+            /Line 1, col 3:/],
+
+    ["An illegal statement on line 2",
+        "letMeLearnYouSomething(5)!\n" +
+        "x * 5!",
+            /Line 2, col 3:/],
+
+    ["A statement missing an ending exclamation point",
+        "letMeLearnYouSomething(5)",
+        /Line 1, col 26:/],
+
+    ["A statement following a statement with a missing ending exclamation point",
+        "letMeLearnYouSomething(5)\n" +
+        "letMeLearnYouSomething(3)!",
+        /Line 2, col 1:/],
+
+    ["A statement starting with an open parenthesis",
+        "letMeLearnYouSomething(5)!\n" +
+        ")",
+            /Line 2, col 1:/],
+
+    ["An expression starting with an asterisk",
+        "handful x: * 71!",
+            /Line 1, col 12:/],
+
+    ["Negation before exponentiation",
+        "letMeLearnYouSomething(-2**2)!",
+            /Line 1, col 27:/],
+
+    /**
+     * NOTE: Currently, JohnLang allows JavaScript-like mixing of ands and ors. If we decide
+     * to change this feature, uncomment this test case.
+     */
+    // ["Mixing ors and ands",
+    //     "letMeLearnYouSomething(1 || 2 && 3)!",
+    //         /Line 1, col 31:/],
+
+    ["Associating relational operators",
+        "letMeLearnYouSomething(1 < 2 < 3)!",
+            /Line 1, col 30:/],
+
+    ["While without braces",
+        "holdMyBeer youBetcha\n" +
+        "letMeLearnYouSomething(1)!",
+            /Line 2, col 1/],
+
+    ["If without braces",
+        "ope x < 3\n" +
+        "letMeLearnYouSomething(1)!",
+            /Line 2, col 1/],
+
+    ["For as identifier",
+        "handful tilTheCowsComeHome: 3!",
+            /Line 1, col 9/],
+
+    ["If as identifier",
+        "handful ope: 8!",
+            /Line 1, col 9/],
+
+    ["Class without constructor",
+        "doohickey Circle {\n" +
+        "   gitErDone diameter(): handful {\n" +
+        "       betterGetGoin 1!\n" +
+        "   }\n"+
+        "}",
+            /Line 2, col 4/],
+
+    ["Class with constructor with return type",
+        "doohickey Circle {\n" +
+        "   slapTogether(handful r): handful {\n" +
+        "       handful me.radius: r!\n" +
+        "   }\n" +
+        "}",
+        /Line 2, col 27/],
+
+    ["Class with code outside of methods",
+        "doohickey Circle {\n" +
+        "   slapTogether(handful r) {\n" +
+        "       handful me.radius: r!\n" +
+        "   }\n" +
+        "   handful me.diameter: me.radius * 2!" +
+        "}",
+            /Line 5, col 4/],
+
+    ["Unbalanced brackets",
+        "todo a: [1!",
+            /Line 1, col 11/],
+
+    ["Bad array literal",
+        "letMeLearnYouSomething([1,2,])!",
+            /Line 1, col 29/],
+
+    ["Bad map literal",
+        "letMeLearnYouSomething({a: 1, b:})!",
+            /Line 1, col 33/],
+
+    ["Empty subscript",
+        "letMeLearnYouSomething(a[])!",
+            /Line 1, col 26/],
+
+    ["Assigning to bool literal",
+        "youBetcha: 1!",
+            /Line 1, col 10/],
+
+    ["Assigning to num literal",
+        "1: 2!",
+            /Line 1, col 2/],
+
+    ["Subscripting number literals",
+        "letMeLearnYouSomething(500[x])!",
+            /Line 1, col 27/],
+
+    ["Subscripting bool literals",
+        "letMeLearnYouSomething(youBetcha[x])!",
+        /Line 1, col 33/],
+
+    ["Calling number literals",
+        "letMeLearnYouSomething(500(x))!",
+            /Line 1, col 27/],
+
+    ["Calling string literals",
+        "letMeLearnYouSomething(\"hello\"(x))!",
+        /Line 1, col 31/],
+
+    ["Accessing members in number literals",
+        "letMeLearnYouSomething(500.0.x);",
+            /Line 1, col 29/],
+
+    ["String literal with unknown escape",
+        'letMeLearnYouSomething("ab\\zcdef")!',
+            /Line 1, col 28/],
+
+    ["String literal with code point too long",
+        'letMeLearnYouSomething("\\u{1111111}")!',
+            /Line 1, col 34/],
 ]
 
 describe("Parsing tests", () => {
