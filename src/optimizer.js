f20d74e06381113f4921a53fff6d9286f11b7d1f
Adiro777 <112520123+Adiro777@users.noreply.github.com>
2025-04-23 13:18:51 -0700

    Stubbed out optimizer

--- src/optimizer.js
+++ src/optimizer.js
@@ -1,4 +1,70 @@
+import * as core from "./core.js";
+
 export default function optimize(node) {
     // TODO: Implement
-    return node;
+    return optimizers?.[node.kind]?.(node) ?? node;
+}
+
+const optimizers = {
+    Program(p) {
+        p.statements = p.statements.flatMap(optimize)
+        return p
+    },
+
+    VarDec(s) {
+        return s
+    }, 
+
+    FunDec(f) {
+        return f
+    }, 
+
+    Assignment(s) {
+        return s
+
+    }, 
+
+    Print(s) {
+        return s
+    }, 
+
+    IfStmt(s) {
+        return s
+    }, 
+    
+    LoopStmt(s) {
+        return s
+    }, 
+
+    Call(s) {
+        return s
+    },  
+
+    DotCall(s) {
+        return s
+    },  
+
+    DotExp(s) {
+        return s
+    }, 
+
+    ClassDec(s) {
+        return s
+    }, 
+
+    Return(s) {
+        return s
+    }, 
+
+    Break(s) {
+        return s
+    },   
+
+    CheckStmt(s) {
+        return s
+    }, 
+
+    ThrowStmt(s) {
+        return s
+    }
 }
