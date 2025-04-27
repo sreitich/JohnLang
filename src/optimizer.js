2d7fc8acc6261474f1989d600b52893a0450c0dc
AllenBoyce <allenboyceiii@gmail.com>
2025-04-27 16:25:09 -0700

    Draft of assignment optimization

--- src/optimizer.js
+++ src/optimizer.js
@@ -1,70 +1,72 @@
 import * as core from "./core.js";
 
 export default function optimize(node) {
-    // TODO: Implement
-    return optimizers?.[node.kind]?.(node) ?? node;
+  // TODO: Implement
+  return optimizers?.[node.kind]?.(node) ?? node;
 }
 
 const optimizers = {
-    Program(p) {
-        p.statements = p.statements.flatMap(optimize)
-        return p
-    },
+  Program(p) {
+    p.statements = p.statements.flatMap(optimize);
+    return p;
+  },
 
-    VarDec(s) {
-        return s
-    }, 
+  VarDec(s) {
+    return s;
+  },
 
-    FunDec(f) {
-        return f
-    }, 
+  FunDec(f) {
+    return f;
+  },
 
-    Assignment(s) {
-        return s
+  Assignment(s) {
+    s.source = optimize(s.source);
+    s.target = optimize(s.target);
+    if (s.source == s.target) return [];
+    return s;
+  },
 
-    }, 
+  Print(s) {
+    return s;
+  },
 
-    Print(s) {
-        return s
-    }, 
+  IfStmt(s) {
+    return s;
+  },
 
-    IfStmt(s) {
-        return s
-    }, 
-    
-    LoopStmt(s) {
-        return s
-    }, 
+  LoopStmt(s) {
+    return s;
+  },
 
-    Call(s) {
-        return s
-    },  
+  Call(s) {
+    return s;
+  },
 
-    DotCall(s) {
-        return s
-    },  
+  DotCall(s) {
+    return s;
+  },
 
-    DotExp(s) {
-        return s
-    }, 
+  DotExp(s) {
+    return s;
+  },
 
-    ClassDec(s) {
-        return s
-    }, 
+  ClassDec(s) {
+    return s;
+  },
 
-    Return(s) {
-        return s
-    }, 
+  Return(s) {
+    return s;
+  },
 
-    Break(s) {
-        return s
-    },   
+  Break(s) {
+    return s;
+  },
 
-    CheckStmt(s) {
-        return s
-    }, 
+  CheckStmt(s) {
+    return s;
+  },
 
-    ThrowStmt(s) {
-        return s
-    }
-}
+  ThrowStmt(s) {
+    return s;
+  },
+};
