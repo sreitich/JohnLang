713bc06b98a59006e64de561ae13237c1ce01a33
Adiro777 <112520123+Adiro777@users.noreply.github.com>
2025-04-28 18:45:44 -0700

    added if statement optimization + function flatmaps

--- src/optimizer.js
+++ src/optimizer.js
@@ -23,11 +23,11 @@ const optimizers = {
   },
 
   VariableDeclaration(d) {
-   
+    return d;
   },
 
   Variable(v) {
-   
+    return v;
   },
 
   AssignmentStatement(s) {
@@ -38,19 +38,19 @@ const optimizers = {
   },
 
   FunctionDeclaration(d) {
-   
+    return d;
   },
 
   Function(f) {
-   
+    if (f.body) f.body = f.body.flatMap(optimize);
   },
 
   FunctionType(t) {
-   
+    return t;
   },
 
   ReturnStatement(s) {
-   
+    return s;
   },
 
   PrintStatement(s) {
@@ -59,123 +59,99 @@ const optimizers = {
   },
 
   FunctionCall(c) {
-   
+    return c;
   },
 
   IfStatement(s) {
-   
+    s.test = optimize(s.test);
+    s.consequent = optimize(s.consequent);
+
+    if (s.alternate?.kind?.endsWith?.("IfStatement")) {
+      s.alternate = optimize(s.alternate);
+    } else {
+      s.alternate = s.alternate.flatMap(optimize);
+    }
+
+    if (s.test.constructor === Boolean) {
+      return s.test ? s.consequent : s.alternate;
+    }
+    return s;
   },
 
   ShortIfStatement(s) {
     s.test = optimize(s.test);
     s.consequent = s.consequent.flatMap(optimize);
-    if(s.test.constructor === Boolean) {
-        return s.test ? s.consequent : [];
+    if (s.test.constructor === Boolean) {
+      return s.test ? s.consequent : [];
     }
     return s;
   },
 
   WhileStatement(s) {
     s.test = optimize(s.test);
-    if(s.test === false) {
-        return []
+    if (s.test === false) {
+      return [];
     }
     s.body = s.body.flatMap(optimize);
 
     return s;
   },
 
-  ForStatement(s) {
-   
-  },
+  ForStatement(s) {return s;},
 
-  BreakStatement(s) {
-   
-  },
+  BreakStatement(s) {return s;},
 
-  ClassDeclaration(d) {
-   
-  },
+  ClassDeclaration(d) {return d;},
 
-  ConstructorDeclaration(d) {
-   
-  },
+  ConstructorDeclaration(d) {return d;},
 
-  FieldDeclaration(d) {
-   
-  },
+  FieldDeclaration(d) {return d;},
 
-  MethodDeclaration(d) {
-   
-  },
+  MethodDeclaration(d) {return d;},
 
-  ConstructorCall(c) {
-   
-  },
+  ConstructorCall(c) {return c;},
 
-  MemberExpression(ed) {
-   
-  },
+  MemberExpression(e) {return e;},
 
-  MemberCall(c) {
-   
-  },
+  MemberCall(c) {return c;},
 
   BinaryExpression(e) {
     e.op = optimize(e.op);
     e.left = optimize(e.left);
     e.right = optimize(e.right);
 
-    if([Number, BigInt].includes(e.right.constructor)) {
-        if(e.op === "+") return e.left + e.right;
-        if(e.op === "-") return e.left - e.right;
-        if(e.op === "/") return e.left / e.right;
-        if(e.op === "*") return e.left * e.right;
-        if(e.op === "%") return e.left % e.right;
-        if(e.op === "<=") return e.left <= e.right;
-        if(e.op === "<") return e.left < e.right;
-        if(e.op === "==") return e.left === e.right;
-        if(e.op === "!=") return e.left != e.right;
-        if(e.op === ">=") return e.left >= e.right;
-        if(e.op === ">") return e.left > e.right;
-
-
-    } else if(e.op === "||") {
-        if(e.left === true) return true;
-        if(e.right === true) return true;
-    } else if(e.op === "&&") {
-        if(e.left === false) return false;
-        if(e.right === false) return false;
+    if ([Number, BigInt].includes(e.right.constructor)) {
+      if (e.op === "+") return e.left + e.right;
+      if (e.op === "-") return e.left - e.right;
+      if (e.op === "/") return e.left / e.right;
+      if (e.op === "*") return e.left * e.right;
+      if (e.op === "%") return e.left % e.right;
+      if (e.op === "<=") return e.left <= e.right;
+      if (e.op === "<") return e.left < e.right;
+      if (e.op === "==") return e.left === e.right;
+      if (e.op === "!=") return e.left != e.right;
+      if (e.op === ">=") return e.left >= e.right;
+      if (e.op === ">") return e.left > e.right;
+    } else if (e.op === "||") {
+      if (e.left === true) return true;
+      if (e.right === true) return true;
+    } else if (e.op === "&&") {
+      if (e.left === false) return false;
+      if (e.right === false) return false;
     }
   },
 
-  UnaryExpression(e) {
-   
-  },
+  UnaryExpression(e) {return e;},
 
-  SubscriptExpression(e) {
-   
-  },
+  SubscriptExpression(e) {return e;},
 
-  ArrayExpression(e) {
-   
-  },
+  ArrayExpression(e) {return e;},
 
-  MapExpression(e) {
-   
-  },
+  MapExpression(e) {return e;},
 
-  MemMapEntryberCall(c) {
-   
-  },
+  MemMapEntryberCall(c) {return c;},
 
-  CheckStatement(s) {
-   
-  },
+  CheckStatement(s) {return s;},
 
-  ThrowStatement(s) {
-   
-  },
-
-  
+  ThrowStatement(s) {return s;},
 };
