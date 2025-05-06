39902ba967783deace7d3b91f86c928e3defbb3f
Sam Reitich <sam.reitich@gmail.com>
2025-05-05 21:01:21 -0700

    Added optimization back into generator tests
    This adds a considerable amount of necessary coverage to the optimizer, and reveals new bugs in the optimizer implementation.

--- test/generator.test.js
+++ test/generator.test.js
@@ -5,11 +5,6 @@ import analyze from "../src/analyzer.js"
 import optimize from "../src/optimizer.js"
 import generate from "../src/generator.js"
 
-/**
- * NOTE: These tests are run WITHOUT the optimizer. This is done so we can more clearly map source code to generated
- * code, without worrying about the optimizer affecting our output.
- */
-
 function dedent(s) {
   return `${s}`.replace(/(?<=\n)\s+/g, "").trim()
 }
@@ -49,6 +44,7 @@ const fixtures = [
       switcheroo flag: youBetcha!
       letMeLearnYouSomething("Hello, world")!
       letMeLearnYouSomething(x % 2 == 0)!
+      letMeLearnYouSomething(x * -1 == -x)!
       letMeLearnYouSomething("test" + "case")!
       letMeLearnYouSomething(123)!
       letMeLearnYouSomething(nah flag)!
@@ -58,6 +54,7 @@ const fixtures = [
       let flag_2 = true;
       console.log("Hello, world");
       console.log(((x_1 % 2) === 0));
+      console.log(((x_1 * -1) === -(x_1)));
       console.log(("test" + "case"));
       console.log(123);
       console.log(!(flag_2));
@@ -165,42 +162,54 @@ const fixtures = [
   {
     name: "if_stmts",
     source: `
-      handful x: 100!
-      switcheroo ready: youBetcha!
-      ope ready {
-        letMeLearnYouSomething("go!")!
+      handful x: 0!
+      ope x == 0 {
+        letMeLearnYouSomething(1)!
       }
-      ope x >= 0 {
-        letMeLearnYouSomething("positive")!
-      } welp {
-        letMeLearnYouSomething("negative")!
+      ope x == 0 {
+        letMeLearnYouSomething(1)!
       }
-      ope x > 50 {
-        letMeLearnYouSomething(2)!
-      } welp ope x > 25 {
+      ope x == 0 {
         letMeLearnYouSomething(1)!
       } welp {
-        letMeLearnYouSomething(0)!
+        letMeLearnYouSomething(2)!
+      }
+      ope x == 0 {
+        letMeLearnYouSomething(1)!
+      } welp ope x == 1 {
+        letMeLearnYouSomething(2)!
+      }
+      ope x == 0 {
+        letMeLearnYouSomething(1)!
+      } welp ope x == 1 {
+        letMeLearnYouSomething(2)!
+      } welp {
+        letMeLearnYouSomething(3)!
       }
     `,
     expected: dedent`
-      let x_1 = 100;
-      let ready_2 = true;
-      if (ready_2) {
-        console.log("go!");
+      let x_1 = 0;
+      if ((x_1 === 0)) {
+        console.log("1");
       }
-      if ((x_1 >= 0)) {
-        console.log("positive");
-      } else {
-        console.log("negative");
-      }
-      if ((x_1 > 50)) {
-        console.log(2);
-      } else
-      if ((x_1 > 25)) {
+      if ((x_1 === 0)) {
         console.log(1);
       } else {
-        console.log(0);
+        console.log(2);
+      }
+      if ((x_1 === 0)) {
+        console.log(1);
+      } else
+        if ((x_1 === 2)) {
+          console.log(3);
+        }
+      if ((x_1 === 0)) {
+        console.log(1);
+      } else
+      if ((x_1 === 2)) {
+        console.log(3);
+      } else {
+        console.log(4);
       }
     `
   },
@@ -364,8 +373,7 @@ const fixtures = [
 describe("The code generator", () => {
   for (const fixture of fixtures) {
     it(`produces expected js output for the ${fixture.name} program`, () => {
-      // Run without the optimizer.
-      const actual = generate(analyze(parse(fixture.source)))
+      const actual = generate(optimize(analyze(parse(fixture.source))))
       assert.deepEqual(actual, fixture.expected)
     })
   }
